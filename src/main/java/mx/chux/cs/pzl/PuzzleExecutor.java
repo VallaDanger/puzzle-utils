20de768773ebd7f510449363848f77fa0db179c0
chux <valladanger+chux@gmail.com>
2021-09-17 03:59:02 -0500

    implemented timed and ticker for brute-force solutions
    Change-Id: I071cab364c91a4cf18a144a0617248635712f8e7

--- src/main/java/mx/chux/cs/pzl/PuzzleExecutor.java
+++ src/main/java/mx/chux/cs/pzl/PuzzleExecutor.java
@@ -2,8 +2,9 @@ package mx.chux.cs.pzl;
 
 import java.time.Instant;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
-public class PuzzleExecutor<T> {
+public class PuzzleExecutor<T> implements Supplier<Puzzle<T>> {
 
     public static <T> PuzzleExecutor<T> from(final Puzzle<T> puzzle) {
         return new PuzzleExecutor<T>(puzzle);
@@ -15,18 +16,53 @@ public class PuzzleExecutor<T> {
     private PuzzleExecutor(final Puzzle<T> puzzle) {
         this.puzzle = puzzle;
         this.ticker = new AtomicInteger(0);
+        this.puzzle.setTicker(this.ticker);
+    }
+    
+    @Override
+    public Puzzle<T> get() {
+        return this.puzzle;
     }
 
     public T execute() {
-        this.puzzle.setTicker(this.ticker);
-        return this.puzzle.optimalSolution();
+        return execute(true);
     }
 
+    public T execute(final boolean optimal) {
+        resetTicker();
+        return exec(optimal);
+    }
+    
     public TimedPuzzleSolution<T> executeTimed() {
+        return executeTimed(true);
+    }
+    
+    public TimedPuzzleSolution<T> executeTimed(final boolean optimal) {
+        resetTicker();
         final Instant start = Instant.now();
-        final T solution = execute();
+        final T solution = exec(optimal);
         final Instant finish = Instant.now();
         return TimedPuzzleSolution.from(solution, this.ticker.get(), start, finish);
+    }
+    
+    public int ticks() {
+        return this.ticker.get();
+    }
+    
+    private T exec(final boolean executeOptimal) {
+        return executeOptimal? getOptimalSolution() : getBruteForceSolution();
+    }
+    
+    private void resetTicker() {
+        this.ticker.set(0);
+    }
+    
+    private T getOptimalSolution() {
+        return this.puzzle.optimalSolution();
+    }
+    
+    private T getBruteForceSolution() {
+        return this.puzzle.bruteForceSolution();
     }
 
 }
